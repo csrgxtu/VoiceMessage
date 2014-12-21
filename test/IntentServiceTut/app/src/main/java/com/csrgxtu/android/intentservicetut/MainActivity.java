f1e5b03f6924e9b34ecf44e93517da9f022cb6fa
csrgxtu <archerreilly@gmail.com>
2014-12-21 22:07:18 +0800

    IntentServiceTut dont work, for the illustrations isnt clear

---
+++ test/IntentServiceTut/app/src/main/java/com/csrgxtu/android/intentservicetut/MainActivity.java
@@ -0,0 +1,59 @@
+package com.csrgxtu.android.intentservicetut;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /*
+         * Creates a new Intent to start the RSSPullService
+         * IntentService. Passes a URI in the
+         * Intent's "data" field.
+         */
+        String dataUrl = "http://www.52nlp.cn/feed";
+        Intent mServiceIntent = new Intent(this, RSSPullService.class);
+        mServiceIntent.setData(Uri.parse(dataUrl));
+        this.startService(mServiceIntent);
+
+        Context context = getApplicationContext();
+        CharSequence text = "Hello toast!";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
